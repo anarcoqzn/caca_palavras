4371fff00c1f57840d4d01f13b56602f03afeaf6
Mainara <mainara.farias@ccc.ufcg.edu.br>
2017-07-18 20:27:32 -0300

    recebe palavras e faz verificações

--- Haskell/caca_palavras.hs
+++ Haskell/caca_palavras.hs
@@ -1,45 +1,66 @@
 import System.Random (randomRIO)
 
 main = do 
+		-- Informacoes
         putStrLn " "
         putStrLn "*********************JOGO DE CAÇA PALAVRAS*********************"
         putStrLn " "
         putStrLn "Intruções: "
-        putStrLn "   1 - Você irá digitar o número de palavras que devem ser escondidas no caça palavras."
+        putStrLn "   1 - Você irá digitar as 4 palavras que serão escondidas."
         putStrLn "   2 - Você deverá digitar as palavras que serão escondidas uma por vez."
+        putStrLn "        - As palavras não devem possuir inteiros."
+        putStrLn "        - As palavras devem possuir entre 2 e 10 letras."
+        putStrLn "        - Não deve possuir palavras repetidas."
         putStrLn "   3 - Para procurar as palavras você irá precisar informar as linhas e as colunas das palavras que será formada."
         putStrLn "   4 - O jogo será finalizado quando todas as palavras forem encontradas."
         putStrLn " "
-        putStrLn "Digite a quantidade de palavras que serão escondidas:"
-        x <- getLine
-        let qnt_palavras = read x :: Int
-        valida_n(qnt_palavras)
+        putStrLn "Digite as palavras seguidas de um enter: "
+	
+		
+		-- recebe palavras
+		
+        palavra1 <- getLine
+        verificaInteiro(palavra1)
+        verificaTamanho(palavra1)
+        -- embaralha(palavra1)
         
-        --input <- getLine
-    	--let palavras = (words input)
-	    --imprimeMatriz(caca_palavra)
-        --x <- randomRIO (0,9::Int)
-        --y <- randomRIO (0,9::Int)
-        --putStrLn (show x ++ " e " ++ show y)
+        palavra2 <- getLine
+        verificaInteiro(palavra1)
+        verificaTamanho(palavra2)
+        -- embaralha(palavra2)
+        
+        palavra3 <- getLine
+        verificaInteiro(palavra1)
+        verificaTamanho(palavra3)
+        -- embaralha(palavra3)
+        
+        palavra4 <- getLine
+        verificaInteiro(palavra1)
+        verificaTamanho(palavra4)
+        -- embaralha(palavra4)
+        let palavras = [palavra1, palavra2, palavra3, palavra4]
+        print palavras
+        
         
 caca_palavra = ["","","","","","","","","",""]
 
-valida_n :: Int -> IO()
-valida_n n
-    | n < 4 = do
-              putStrLn "A quantidade de palavras deve ser maior ou igual a 4, tente outra quantidade."   
-              x <- getLine
-              let qnt_palavras = read x :: Int
-              valida_n(qnt_palavras) 
-    | n > 25 = do
-               putStrLn "A quantidade de palavras deve ser menor ou igual a 25, tente outra quantidade."
-               x <- getLine
-               let qnt_palavras = read x :: Int
-               valida_n(qnt_palavras)
-    | otherwise = putStrLn ""
+verificaInteiro :: [Char] -> IO()
+verificaInteiro [] = putStrLn ""
+verificaInteiro (c:cs) 
+	| (c == '0') = error "ERRO: palavra possui inteiro."
+    | (c == '1') = error "ERRO: palavra possui inteiro."
+    | (c == '2') = error "ERRO: palavra possui inteiro."
+    | (c == '3') = error "ERRO: palavra possui inteiro."
+    | (c == '4') = error "ERRO: palavra possui inteiro."
+    | (c == '5') = error "ERRO: palavra possui inteiro."
+    | (c == '6') = error "ERRO: palavra possui inteiro."
+    | (c == '7') = error "ERRO: palavra possui inteiro."
+    | (c == '8') = error "ERRO: palavra possui inteiro."
+    | (c == '9') = error "ERRO: palavra possui inteiro."
+    | otherwise = verificaInteiro(cs)
 
---imprimeMatriz :: [String] -> IO()
---imprimeMatriz [] = putStrLn""
---imprimeMatriz (m:ms) = do
-	--putStrLn m
-	--imprimeMatriz(ms)
+verificaTamanho :: [Char] -> IO()
+verificaTamanho palavra
+	| length palavra < 2 = error "ERRO: palavra possui menos de 2 letras."
+	| length palavra > 10 = error "ERRO: palavra possui mais de 10 letras."
+	| otherwise = putStrLn ""
